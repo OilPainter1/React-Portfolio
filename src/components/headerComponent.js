d51a072d0511bc63c0e28e434188f436c90a97e2
OilPainter1 <hjcryns@gmail.com>
2023-01-25 13:52:00 -0600

    first commit

---
+++ src/components/headerComponent.js
@@ -0,0 +1,10 @@
+import Info from "./infoBlock"
+
+function MakeHeader() {
+    return <header className='navBar'>
+            <Info info="Henry"></Info>
+        </header>
+        
+}
+
+export default MakeHeader
---
+++ src/components/infoBlock.js
@@ -0,0 +1,6 @@
+function Info(info){
+    return <div>
+        <h2>{info.info}</h2>
+        </div>
+}
+export default Info
