133d237c9f045c54d5e412c48f98600cac5f2023
OilPainter1 <hjcryns@gmail.com>
2023-03-08 14:27:56 -0600

    changed contact page

--- src/components/headerComponent.js
+++ src/components/headerComponent.js
@@ -14,6 +14,7 @@ import Resume from "../views/Resume"
 
 
 
+
 function MakeHeader() {
 
     const[view,setView] = useState("About Me")
--- src/views/contactMe.js
+++ src/views/contactMe.js
@@ -1,48 +1,15 @@
 
-function SubmitBtn() {
- 
-        return <button type="submit"
-        className="btn btn-primary"
-        disabled>
-            Submit</button>
-
-}
 
 
 
 function ContactMe() {
   
+   
+
+
 
     return (<div className="container">
-        <strong>Contact Me</strong>
-        <p>(please fill out all fields)</p>
-        <hr></hr>
-        <form>
-       <div className="mb-3">
-           <label htmlFor="form-name" className="form-label">
-               Name:
-           </label>
-           <input type="text" className="form-control" id="form-name">
-           </input>
-       </div>
-       <div className="mb-3">
-            <label htmlFor="form-email" className="form-label">
-                Email:
-            </label>
-            <input type="text" className="form-control" id="form-email">
-            </input>
-       </div>
-       <div className="mb-3">
-            <label htmlFor="form-message" className="form-label">
-                Message:
-            </label>
-            <textarea className="form-control" id="form-message" rows="4"></textarea>
-       </div>
-
-        
-        {SubmitBtn()}
-
-   </form>
+        <a href="mailto:hjcryns@gmail.com">Email me</a>
     </div>)
 
 }
